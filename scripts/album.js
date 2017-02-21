ff90e82f0092ec796b07e437345e8d3966488bbd
Rachel Kay Kingswood <rachelkaykingswood@gmail.com>
2017-02-20 20:34:45 -0500

    Fix weird function embedding from lines 109 - 129.

--- scripts/album.js
+++ scripts/album.js
@@ -100,12 +100,13 @@ var waitForClick = document.getElementsByClassName('album-cover-art')[0].addEven
         if (event.target.parentElement.className === 'album-view-song-item' && getSongItem(event.target).getAttribute('data-song-number') !== currentlyPlayingSong) {
              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
          }
+    });
          
-          songListContainer.addEventListener('click', function(event){
+    songListContainer.addEventListener('click', function(event){
                clickHandler(event.target);
      });
 
-        for (var i = 0; i < songRows.length; i++) {
+    for (var i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
            
              var songItem = getSongItem(event.target);
@@ -114,11 +115,8 @@ var waitForClick = document.getElementsByClassName('album-cover-art')[0].addEven
              if (songItemNumber !== currentlyPlayingSong) {
                  songItem.innerHTML = songItemNumber;
              }
-         });
-     }
-});
-
-//I tried writing the whole play/pause functionalities on my own (with very limited success) before I realized that there were more broken down instructions in the checkpoint. When I read them, I didn't understand what the point of the two functions was from the description, and I couldn't write a function if I didn't know what it was supposed to do. So I went ahead and looked at the solutions, which then made clear what they are doing, and *now* I understand the checkpoint well enough to have done it myself. So yes, these are copy/pasted, but if I need to do this again elsewhere, I totally can now. 
+          })
+    };
 
 var findParentByClassName = function(element, targetClass) {
     if (element) {
